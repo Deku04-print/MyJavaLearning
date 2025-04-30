b60c495534a1aa6b145efb1745c1f104b818b399
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-01 02:02:25 +0530

    Array file commited

---
+++ src/ArraysConcept.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArraysConcept {
+
+    public static void main(String[] args){
+    int[] arr={10,20,30,40,50,60,70,80,90};
+
+        int n = arr.length;
+        System.out.println(n);
+
+        Scanner sc=new Scanner(System.in);
+
+        for(int i=0;i<=n;i++){
+
+
+
+
+
+        }
+
+    }
+
+
+
+}
