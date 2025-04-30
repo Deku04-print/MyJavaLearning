5bf09cf7cd28c0e7b287d6ccde1145bccb3cdb17
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-01 02:42:35 +0530

    I have Solved the problem of Finding biggest num

---
+++ src/BiggestNum.java
@@ -0,0 +1,20 @@
+public class BiggestNum {
+
+    public static void main(String[] args){
+        int[] arr={10, 4, 6, 8, 12};
+
+        int n=arr.length;
+        int maxValue=Integer.MIN_VALUE;
+
+        for (int i=0;i<n;i++){
+            int current=arr[i];
+            if(maxValue<current) {
+                maxValue=current;
+            }
+
+
+        }
+        System.out.println("The Biggest Number is :" + maxValue);
+    }
+}
+//maxValue= Math.max(maxValue,arr[i]);
