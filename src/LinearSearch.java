4875fc020856320c80e0e7817ac0292b82261c02
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-01 03:32:07 +0530

    Linear search on Java arrays

---
+++ src/LinearSearch.java
@@ -0,0 +1,37 @@
+//This code find out the index of the Key value which you entered
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LinearSearch {
+
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of the array : ");
+        int n = sc.nextInt();
+        int[] arr=new int[n];
+
+        for (int i = 0; i <n ; i++) {
+            System.out.println("Enter the Element of the array of index  "+i+ " :");
+            arr[i]=sc.nextInt();
+
+        }
+        System.out.println("Array is "+ Arrays.toString(arr));
+
+        System.out.println("Enter the key value to find : ");
+        int key=sc.nextInt();
+
+        int ans =-1;
+        int i;
+        for (i = 0; i < arr.length; i++) {
+            if(arr[i]==key){
+                ans=i;
+                break;
+            }
+
+        }
+        System.out.println("Value is at Index :"+i);
+
+    }
+}
