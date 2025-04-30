55a3a82b2539cc51cc9ab0cee21b226fcd628b43
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-01 03:14:14 +0530

    Input Output opperations on Java arrays

---
+++ src/ArrayOpp.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayOpp {
+
+    public static void main(String[] args){
+
+        int[] arr=new int[5];
+
+        int n=arr.length;
+
+        Scanner sc=new Scanner(System.in);
+         for (int i=0;i<n;i++){
+             System.out.print("Enter the Element :");
+             arr[i]=sc.nextInt();
+         }
+             System.out.println("Element of the Array :");
+         for(int i=0;i<n;i++){
+             System.out.println(arr[i]);
+         }
+        System.out.println("Array is :"+ Arrays.toString(arr));
+    }
+
+}
