33e4e6a80d4a2ff8a68bb224c2448b7fbd301d50
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-03 03:14:56 +0530

    reverse of the String

---
+++ src/StringOperations/RevString.java
@@ -0,0 +1,21 @@
+package StringOperations;
+
+import java.util.Scanner;
+
+public class RevString {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String :");
+
+        String s1=sc.nextLine();
+        String arr="";
+        int n=s1.length()-1;
+        for (int i = n; i>=0 ; i--) {
+
+            arr+=s1.charAt(i);
+
+        }
+        System.out.println("Reverse of the String :"+arr);
+    }
+}
