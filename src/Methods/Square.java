8a229ecc5f8674948eb23c70196a42890ba63c94
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-04 23:52:19 +0530

    Edited the Code , Learned something new

--- src/Methods/Square.java
+++ src/Methods/Square.java
@@ -5,8 +5,19 @@ public class Square {
     static int Squarenum(int num){
         return num*num;
     }
-    public static void main(String[] args){
-        System.out.println("Square of 9 :"+Squarenum(9));
-        System.out.println("Square of 5 :"+Squarenum(5));
+    static String[] heros(){
+        String[] superheros={"Spider Man","Iron Man","Hulk"};
+        return superheros;
+    }
+    public static void main(String[] args) {
+//        System.out.println("Square of 9 :"+Squarenum(9));
+//        System.out.println("Square of 5 :"+Squarenum(5));
+
+        String[] arr=heros();
+
+        for(String heros:arr){
+            System.out.println(heros);
+        }
+
     }
 }
