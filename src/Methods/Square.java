cdba0758386d2764902edee5a6169e9934efde1c
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-04 23:45:41 +0530

    Square of a number using Methods in java

---
+++ src/Methods/Square.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class Square {
+
+    static int Squarenum(int num){
+        return num*num;
+    }
+    public static void main(String[] args){
+        System.out.println("Square of 9 :"+Squarenum(9));
+        System.out.println("Square of 5 :"+Squarenum(5));
+    }
+}
