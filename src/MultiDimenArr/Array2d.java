13cc608a596409fddb787df117b9c95604b2cd9e
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-07 23:16:09 +0530

    MultiDimen array 2d

---
+++ src/MultiDimenArr/Array2d.java
@@ -0,0 +1,20 @@
+package MultiDimenArr;
+
+public class Array2d {
+
+    public static void main(String[] args){
+
+        String[][] groceries = {{"apple", "orange", "banana"},
+                {"potato", "onion", "carrot"},
+                {"chicken", "pork", "beef", "fish"}};
+
+        groceries[2][1] = "eggs";
+
+        for(String[] foods : groceries){
+            for(String food : foods){
+                System.out.print(food + " ");
+            }
+            System.out.println();
+        }
+    }
+}
