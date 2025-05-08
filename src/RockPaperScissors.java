b0007b7d566d5e38803de7b8116d99510956007b
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-09 03:52:28 +0530

    RockPaperScissors Game Final commit...

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -14,11 +14,38 @@ public class RockPaperScissors {
 
         String[] Choice= {"rock","paper","scissor"};
 
-        PlayerChoice=sc.next().toLowerCase();
+        do {
+            System.out.println("Enter a choice :");
+            PlayerChoice=sc.next().toLowerCase();
 
-        if (!PlayerChoice.equals("rock") &&
-                (!PlayerChoice.equals("paper") &&
-                    (!PlayerChoice.equals("scissor")
+            if (!PlayerChoice.equals("rock") &&
+                    !PlayerChoice.equals("paper") &&
+                    !PlayerChoice.equals("scissor")){
+                System.out.println("Invalid choice....!!");
+
+            }
+
+            ComputerChoice=Choice[random.nextInt(3)];
+            System.out.println("Computer CHoice :"+ComputerChoice.toUpperCase());
+
+            if (ComputerChoice.equals(PlayerChoice)){
+                System.out.println("It's A TIE ");
+            } else if (PlayerChoice.equals("rock") && ComputerChoice.equals("scissor") ||
+                    PlayerChoice.equals("scissor") && ComputerChoice.equals("paper") ||
+                    PlayerChoice.equals("paper") && ComputerChoice.equals("rock")){
+
+                System.out.println("Stand Proud....!! it's a WIN ");
+
+            }else {
+                System.out.println("Youu Lose Bastard......>..<");
+            }
+
+            System.out.println("Do you want to Play again....??");
+            System.out.println("Input -- >  Yes / No");
+            PlayAgain=sc.next().toLowerCase();
+        }while (PlayAgain.equals("yes"));
+
+        System.out.println("Thanks For playing budd...!! Seee yaa.....");
 
 
     }
