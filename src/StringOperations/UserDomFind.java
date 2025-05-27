bf11181d2d1d59a6133da43076ddcfc5862bcdeb
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-27 23:28:15 +0530

    I have upgraded the email user name and domain file solution

--- src/StringOperations/UserDomFind.java
+++ src/StringOperations/UserDomFind.java
@@ -12,12 +12,17 @@ public class UserDomFind {
         String username;
         System.out.print("Enter your Email :");
         email=sc.nextLine();
+        if(email.contains("@")){
 
-        username=email.substring(0,email.indexOf("@"));
-        domain=email.substring(email.indexOf("@")+1);
+            username=email.substring(0,email.indexOf("@"));
+            domain=email.substring(email.indexOf("@")+1);
 
-        System.out.println("Username Is :"+username);
-        System.out.println("Domain is :"+domain);
+            System.out.println("Username Is :"+username);
+            System.out.println("Domain is :"+domain);
+        }else{
+            System.out.println("You have entered a invalid email...!");
+        }
+
 
         sc.close();
 
