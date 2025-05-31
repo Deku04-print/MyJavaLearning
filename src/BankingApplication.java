fbfcd6322a94e42e3cabac0d1314737000a5c167
Harsh dabhade <harshdabhade8867@gmail.com>
2025-05-31 23:05:01 +0530

    Seting up the procedure

---
+++ src/BankingApplication.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BankingApplication {
+
+    public static void main(String[] args){
+
+         Scanner sc = new Scanner(System.in);
+
+         //variable declaration
+
+
+
+        //welcome msg
+
+        //User input
+
+        //deposit Money()
+
+         //Show Balance()
+
+        //withdraw money()
+
+        //exit()
+
+    }
+
+}
