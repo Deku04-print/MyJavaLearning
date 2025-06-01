6205fc550e2b395ba0023244d2177d7635bce804
Harsh dabhade <harshdabhade8867@gmail.com>
2025-06-02 04:17:02 +0530

    Table program enhancements

--- src/LoopLearningProblems/LoopFor.java
+++ src/LoopLearningProblems/LoopFor.java
@@ -1,9 +1,17 @@
 package LoopLearningProblems;
 
+import java.util.Scanner;
+
 public class LoopFor {
     public static void main(String[] Args){
-        for(int i=3;i<=30;i+=3){
-            System.out.println("Value of i :"+i);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Welcome to Table Printer...!!!");
+        System.out.println("Enter the Number to print the table :");
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n + "*"+ i + "= " + (n*i));
         }
+
     }
 }
