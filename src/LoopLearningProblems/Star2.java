7c81b1bfbc1acbd7fc21c861ba112a0523a99d0f
Harsh dabhade <harshdabhade8867@gmail.com>
2025-06-02 04:39:43 +0530

    Added and corrected the star pattern files

--- src/LoopLearningProblems/Star2.java
+++ src/LoopLearningProblems/Star2.java
@@ -2,18 +2,20 @@ package LoopLearningProblems;
 
 import java.util.Scanner;
 
-public class StarPattern {
-    //Star pattern program solved
+public class Star2 {
+    //Reverse Star Pattern
     public static void main(String[] args){
         System.out.println("Welcome to Star pattern Printer...");
-        System.out.println("Please enter the value to print stars :");
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lines value :");
         int n = sc.nextInt();
-        for (int i=1;i<=n;i++){
+        for (int i=n;i>=0;i--){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+
+
 }
