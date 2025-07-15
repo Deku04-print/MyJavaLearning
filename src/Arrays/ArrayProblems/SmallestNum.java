b620ed823e23fa47f8c0782d785ee77e76ec43f1
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-07-16 03:01:16 +0530

    Solving the Fibonacci problem

--- src/Arrays/ArrayProblems/SmallestNum.java
+++ src/Arrays/ArrayProblems/SmallestNum.java
@@ -1,22 +1,20 @@
-package ArrayProblems;
+package Arrays.ArrayProblems;
 
-public class BiggestNum {
+public class SmallestNum {
 
     public static void main(String[] args){
-        int[] arr={10, 4, 6, 8, 12};
+
+        int[] arr ={2, 5, 1, 7, 3};
 
         int n=arr.length;
-        int maxValue=Integer.MIN_VALUE;
+        int maxValue=Integer.MAX_VALUE;
 
         for (int i=0;i<n;i++){
             int current=arr[i];
-            if(maxValue<current) {
+            if(maxValue>current) {
                 maxValue=current;
             }
-
-
         }
         System.out.println("The Biggest Number is :" + maxValue);
     }
 }
-//maxValue= Math.max(maxValue,arr[i]);
