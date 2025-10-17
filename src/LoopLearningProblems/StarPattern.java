a92f70fc19f642038e37139e2e006a04e83c27ec
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-10-17 22:50:52 +0530

    Solved a random problem

--- src/LoopLearningProblems/StarPattern.java
+++ src/LoopLearningProblems/StarPattern.java
@@ -2,18 +2,19 @@ package LoopLearningProblems;
 
 import java.util.Scanner;
 
-public class StarPattern {
-    //Star pattern program solved
+public class StarPattern{
     public static void main(String[] args){
-        System.out.println("Welcome to Star pattern Printer...");
-        System.out.println("Please enter the value to print stars :");
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
+        System.out.println("Welcome to start pattern printer");
+        System.out.println("Enter the Rows for printing star pattern...");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for (int j=n;j>=i;j--){
+                System.out.print("*"+" ");
             }
             System.out.println();
+
         }
     }
 }
--- src/LoopLearningProblems/SumOfposi.java
+++ src/LoopLearningProblems/SumOfposi.java
@@ -6,15 +6,8 @@ public class SumOfposi {
 
     public static void main(String[] args){
 
-        Scanner sc=new Scanner(System.in);
-        int sum=0;
-        int num;
-
-        do{
-            num=sc.nextInt();
-            sum+=num;
-        }while(num>0);
-
-        System.out.println("Sum :"+sum);
+        for(int i=9;i>=1;i--){
+            System.out.print(i+" ");
+        }
     }
 }
