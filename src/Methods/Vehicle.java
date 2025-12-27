51d56542aba7449a1cd6c59b51e0bd449c0e6e54
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2025-12-27 23:06:28 +0530

    Pushing the code

--- src/Methods/Vehicle.java
+++ src/Methods/Vehicle.java
@@ -1,4 +1,8 @@
 package Methods;
 
-public class Vehicle {
+public abstract class Vehicle {
+    //limit for car to accerlate
+    public abstract void accelerate();
+
+    public abstract void decelerate();
 }
