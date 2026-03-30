5a960e6459e8959db29898c4613b53eeff7eef9a
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-03-31 04:38:43 +0530

    file reader experimwnt done

--- src/FileJava/Files.java
+++ src/FileJava/Files.java
@@ -1,24 +1,44 @@
 package FileJava;
 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Files {
     public static void main(String[] args) throws IOException {
         //How to write a file in java
 
 
-        try(FileWriter writer = new FileWriter("Test.txt")){
-            writer.write("I like pizzzzzza");
-            System.out.println("File has been writen");
+//        try(FileWriter writer = new FileWriter("Test.txt")){
+//            writer.write("I like pizzzzzza");
+//            System.out.println("File has been writen");
+//        }
+//        catch (FileNotFoundException e){
+//            System.out.println("Could not locate file location..");
+//        }
+//        catch (IOException e){
+//            System.out.println("Could not write a file");
+//
+//        }
+
+        String filePath = "T:\\Java Training\\Learning\\MyFirstProject\\Test.txt";
+
+
+        try(BufferedReader bf = new BufferedReader(new FileReader(filePath))){
+
+
+//            System.out.println("That file exist...");
+            String line;
+            while ((line = bf.readLine())!=null){
+                System.out.println(line);
+            }
         }
         catch (FileNotFoundException e){
-            System.out.println("Could not locate file location..");
-        }
-        catch (IOException e){
-            System.out.println("Could not write a file");
+            System.out.println("File not found..!!");
 
         }
+        catch (IOException e){
+            System.out.println("Something wentt Wrong....!!");
+
+        }
+
     }
 }
