86c48ea585c2734cf0fdcfceb1f4b062b92b07c6
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-03-31 04:31:01 +0530

    wxpwrimwnt done

--- src/FileJava/Files.java
+++ src/FileJava/Files.java
@@ -1,5 +1,6 @@
 package FileJava;
 
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -11,6 +12,9 @@ public class Files {
         try(FileWriter writer = new FileWriter("Test.txt")){
             writer.write("I like pizzzzzza");
             System.out.println("File has been writen");
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Could not locate file location..");
         }
         catch (IOException e){
             System.out.println("Could not write a file");
