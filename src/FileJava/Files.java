76625ea33ce39ee46335271edd5bb65314007cf8
Harsh D <112274179+Deku04-print@users.noreply.github.com>
2026-03-31 04:05:50 +0530

    File writer learned

--- src/FileJava/Files.java
+++ src/FileJava/Files.java
@@ -1,4 +1,20 @@
 package FileJava;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Files {
+    public static void main(String[] args) throws IOException {
+        //How to write a file in java
+
+
+        try(FileWriter writer = new FileWriter("Test.txt")){
+            writer.write("I like pizzzzzza");
+            System.out.println("File has been writen");
+        }
+        catch (IOException e){
+            System.out.println("Could not write a file");
+
+        }
+    }
 }
